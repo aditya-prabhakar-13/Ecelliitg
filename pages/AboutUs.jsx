8fb7a4551c2d89171aff3bbb25ce037e63f433ef
a-kakade <a.kakade@iitg.ac.in>
2025-12-09 16:17:05 +0530

    AboutHero Added

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import AlumList from "../src/Components/AboutUs/AlumList";
 import TeamsLanding from "../src/Components/AboutUs/TeamsLanding";
 import OurVision from "../src/Components/AboutUs/OurVision.jsx";
+import AboutHero from "../src/Components/AboutUs/AboutHero.jsx";
 export default function AboutUs(){
     return (
         <div className="AboutUs">
             <TeamsLanding />
             <AlumList />
             <OurVision />
+            <AboutHero />
         </div>
     )
 }
